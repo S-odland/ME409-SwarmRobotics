aa0fd58a155c64abefb13d7d19de180ff635c21a
S-odland <49540258+S-odland@users.noreply.github.com>
2020-10-05 10:08:32 -0500

    robot 1 orbit working nicely, robot 0 outputs distance and changes led accordingly, all thats left is commenting for readability

--- Lab1/fs/usr_code.py
+++ Lab1/fs/usr_code.py
@@ -16,7 +16,7 @@ def usr(robot):
 		if len(msgs) > 0:
 			pose_rxed= struct.unpack('ffi', msgs[0][:12])
 			position = robot.get_pose()
-			if position:
+			if position and pose_rxed[2] == 0:
 				rID = robot.id
 				if rID == 1:
 					xdif = position[0] - pose_rxed[0]
@@ -26,26 +26,31 @@ def usr(robot):
 
 					distDif = desired_distance - distance
 
-					if distDif > 0.015:
-						robot.set_vel(69,43)
+					if distDif > 0:
+						robot.set_vel(71,43)
 						robot.set_led(100,0,0)
-					elif distDif < -0.015:
-						robot.set_vel(71,41)
-						robot.set_led(0,100,0)
-					elif 0.015 > distDif > 0:
-						robot.set_vel(70,43)
-						robot.set_led(100,0,0)
-					elif 0 > distDif > -0.015:
-						robot.set_vel(70,41)
+					elif 0 > distDif:
+						robot.set_vel(69,40)
 						robot.set_led(0,100,0)
 					elif distDif == 0:
 						robot.set_vel(70,42)
+					#print(distance)
 
-					robot.send_msg(struct.pack('ff', distDif,distance))
+			if position and pose_rxed[2] == 1:
+				rID = robot.id
+				if rID == 0:
+					xdif = position[0] - pose_rxed[0]
+					ydif = position[1] - pose_rxed[1]
 
-			if robot.id == 0:
-				msg = robot.recv_msg()
-				distns = struct.unpack(msg[0][:8])
+					distance = math.sqrt(xdif**2 + ydif**2)
 
-			print(distns[1])
+					distDif = desired_distance - distance
+
+					if distDif > 0:
+						robot.set_led(100,0,0)
+					elif 0 > distDif:
+						robot.set_led(0,100,0)
+					print(distance)
+
+
 
