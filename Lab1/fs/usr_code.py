6f4e883c0eba7f5a6015624cd541f15af24d12c2
S-odland <49540258+S-odland@users.noreply.github.com>
2020-10-04 22:45:01 -0500

    I think I understand how to have the robots communicate with each other, now onto controlling the drive!

--- Lab1/fs/usr_code.py
+++ Lab1/fs/usr_code.py
@@ -1,23 +1,34 @@
 def usr(robot):
 	import struct
 	import math
-	
+
 	desired_distance = 0.17 #use this variable to set desired distance of orbit
-	
-	while True:
-		pose_t=robot.get_pose()# gets pose The syscall that returns the robot's global pose (x, t, theta).
-		#Note that the onboard sensor has a limited sampling rate of 30HZ. If there is no data from received the sensor since the last time 
-		# the function get_pose() is called, the syscall will return None. If there is new data received from the sensor, the syscall will 
-		# return a 3-tuple (x, y, theta), which are robot's x position, y, position, and orientation, respectively.
+	i = 0
+	while i <= 100:
+		i += 1
 
-		if pose_t: #check pose is valid before using
+		pose_t=robot.get_pose()
+
+		if pose_t: 
 			pose=pose_t
-			print('The x,y postion of robot ',robot.id,' is ', pose[0],pose[1])
-			robot.send_msg(struct.pack('ffi', pose[0], pose[1],robot.id))# send pose x,y in message
-		
-
+			robot.send_msg(struct.pack('ffi', pose[0], pose[1],robot.id))
+			
 		msgs = robot.recv_msg()
 		if len(msgs) > 0:
 			pose_rxed= struct.unpack('ffi', msgs[0][:12])
-			print('robot ',robot.id,' received position ',pose_rxed[0],pose_rxed[1],' from robot ', pose_rxed[2])
-			
+			position = robot.get_pose()
+			if position:
+				rID = robot.id
+				if rID == 1
+			## this if allows me to know if robot 1 is to the right or left of robot 0 because position of robot 0 will always be subtracted from robot 1
+					xdif = position[0] - pose_rxed[0]
+					ydif = position[1] - pose_rxed[1]
+
+					distance = math.sqrt(xdif**2 + ydif**2)
+
+					distDif = desired_distance - distance
+
+					print(position[0],rID,position[1])
+					print('robot ',rID,' received position ',pose_rxed[0],pose_rxed[1],' from robot ', pose_rxed[2],i)
+					print(distDif)
+
