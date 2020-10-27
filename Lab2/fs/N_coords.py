53e5e50f0740f4cb00212fab66eafc5fbea82cf5
S-odland <49540258+S-odland@users.noreply.github.com>
2020-10-26 22:30:21 -0500

    N has been formed, just mirrored about its y-axis, mu py code to make coords in N, more work to be done

---
+++ Lab2/fs/N_coords.py
@@ -0,0 +1,26 @@
+from PIL import Image
+
+filename = 'N.jpg'
+
+## size = 186 x 258
+
+img = Image.open(filename)
+newsize = (30,36)
+img = img.resize(newsize)
+
+coordinate1 = 1,1
+img = img.convert('1')
+img.show()
+
+coordinates = []
+
+for i in range(0,30):
+    for j in range(0,36):
+
+        coordinate = i,j
+        if img.getpixel(coordinate) == 0:
+            coordinates.append(coordinate)
+
+list_of_lists = [list(elem) for elem in coordinates]
+print(len(coordinates))
+print(list_of_lists)
